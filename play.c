ad7567398830b2d9b84ce6a6f1797fd0be933aad
Faiyam Rahman <frahman305@gmail.com>
2014-08-15 16:14:24 -0400

    moved song file parsing to a seperate function

--- play.c
+++ play.c
@@ -8,44 +8,36 @@
 /* handle errors produced by port audio functions */
 int handlePaError(PaError err); 
 
+/* Extract song data into an array */
+int getAudioData(const char *file, AIFF_Ref *song, int **sampleArray, uint64_t *nSamples);
+
+/* Clean song parameters after playing the file */
+int cleanSong(int *soundSamples, AIFF_Ref *song);
+
 /* play the AIFF referenced by argv[1]
      Usage: play musicFile.aiff */
 int main(int argc, char *argv[]) {
 
-    /* portaudio and libaiff parameters */
+    /* portaudio and AIFF parameters */
     PaError err;
     PaStream *stream;
-    int buffer[FRAMES_PER_BUFFER][2];       // stereo output buffer
     AIFF_Ref song;
-
-    /* Open the songfile */
+    int buffer[FRAMES_PER_BUFFER][2];           // stereo output buffer
+    
+    /* Open the songfile and get sound Samples */
+    int *soundSamples;
+    uint64_t numSamples;
     const char *songFile = argv[1];
-    song = AIFF_OpenFile(songFile, F_RDONLY);
-    if (!song) 
+    if (getAudioData(songFile, &song, &soundSamples, &numSamples) < 0)
     {
-        fprintf( stderr, "Failed to open file %s\n", songFile);
-        AIFF_CloseFile(song);
+        fprintf(stderr, "Could not get audio data from %s\n", songFile);
         abort();
     }
-    printf("Opened file succesfully\n");
-
-    /* Get sound format info */
-    uint64_t seconds = 0;
-    uint64_t nSamples = 0;
-    double samplingRate = 0;
-    int channels = 0;
-    int bitsPerSample = 0;
-    int segmentSize = 0;
-    if (AIFF_GetAudioFormat(song, &nSamples, &channels, &samplingRate, &bitsPerSample, &segmentSize) < 1) 
-    {
-        fprintf( stderr, "Could not get audio format information\n");
-        AIFF_CloseFile(song);
-        abort();
-    }
+    printf("We got audio data succesfully\n");
 
     /* Open a stream to play the song */
-    err = Pa_Initialize();
-    if ( err != paNoError ) { handlePaError(err); }
+    if ((err = Pa_Initialize()) != paNoError ) 
+        handlePaError(err);
     err = Pa_OpenDefaultStream(
             &stream,                            // stream
             0,                                  // numInputChannels. 0->output only stream
@@ -55,23 +47,13 @@ int main(int argc, char *argv[]) {
             0,                                  // preferred granularity for blocking read/write stream. 0->let portaudio optimize this 
             NULL,                               // pointer to asynchronous callback function for filling output buffer,
             NULL);                              // pointer passed to callback function
-    if ( err != paNoError ) { handlePaError(err); }
-    printf("Opened audio stream successfully\n");        
-    
-
-    /* Extract audio samples */
-    int numBytes = (2 * nSamples * 4);          // each sample is 4 bytes, AIFF calls a frame a "sample", so we mult by 2
-    int *soundSamples = (int *) malloc(2 * numBytes);
-    if (!soundSamples) 
-    {
-        fprintf( stderr, "soundSamples could not be allocated\n"); abort();
-    }
-    AIFF_ReadSamples32Bit(song, soundSamples, 2 * nSamples);
+    if ( err != paNoError ) 
+        handlePaError(err);      
 
     /* Play the song */
     int loopOffset;
     int frameOffset;
-    int loopLimit = nSamples / FRAMES_PER_BUFFER;   // on each loop, we go through FRAMES_PER_BUFFER "samples" as defined by AIFF
+    int loopLimit = numSamples / FRAMES_PER_BUFFER;   // on each loop, we go through FRAMES_PER_BUFFER "samples" as defined by AIFF
     err = Pa_StartStream(stream);
     if ( err != paNoError ) { handlePaError(err); }
 
@@ -88,8 +70,7 @@ int main(int argc, char *argv[]) {
         if ( err != paNoError ) { handlePaError(err); }
     }
     /* Clean up (Stop stream, close stream, free soundSamples) */
-    free(soundSamples);                                 // Free soundSamples
-    AIFF_CloseFile(song);                               // Close song file
+    cleanSong(soundSamples, &song);
     if ((err = Pa_StopStream(stream)) != paNoError)     // Stop the stream
         handlePaError(err); 
     if ( (err = Pa_CloseStream(stream)) != paNoError )  // Close the stream
@@ -98,21 +79,63 @@ int main(int argc, char *argv[]) {
         handlePaError(err);
     
     /* Print song info */
-    seconds = nSamples / samplingRate;
-    printf("Song Length: %lu:%02lu:%02lu \n", (unsigned long) seconds/360, 
-               (unsigned long) (seconds/60)%60, (unsigned long) seconds%60 );
-    printf("Sampling Rate: %f\n", samplingRate);
-    printf("Number of Channels: %d\n", channels);
-    printf("bit depth: %i\n", bitsPerSample);
-    printf("Num samples: %llu\n", nSamples);
-    printf("FRAMES_PER_BUFFER: %d\n", FRAMES_PER_BUFFER);
-    printf("loopLimit: %d\n", loopLimit);
-    printf("loopLimit as float: %f\n", nSamples / (double) FRAMES_PER_BUFFER);
+    // seconds = nSamples / samplingRate;
+    // printf("Song Length: %lu:%02lu:%02lu \n", (unsigned long) seconds/360, 
+    //            (unsigned long) (seconds/60)%60, (unsigned long) seconds%60 );
+    // printf("Sampling Rate: %f\n", samplingRate);
+    // printf("Number of Channels: %d\n", channels);
+    // printf("bit depth: %i\n", bitsPerSample);
+    // printf("Num samples: %llu\n", nSamples);
+    // printf("FRAMES_PER_BUFFER: %d\n", FRAMES_PER_BUFFER);
+    // printf("loopLimit: %d\n", loopLimit);
+    // printf("loopLimit as float: %f\n", nSamples / (double) FRAMES_PER_BUFFER);
     
     return 0;
 }
 
-int handlePaError(PaError err) {
+int getAudioData(const char *file, AIFF_Ref *song, int **sampleArray, uint64_t *nSamples)
+{
+    /* Open file */
+    *song = AIFF_OpenFile(file, F_RDONLY);
+    if (!*song) 
+    {
+        fprintf( stderr, "Failed to open file %s\n", file);
+        AIFF_CloseFile(*song);
+        return -1;
+    }
+
+    /* Get sound format info */
+    uint64_t seconds;
+    double samplingRate;
+    int channels, bitsPerSample, segmentSize;
+    if (AIFF_GetAudioFormat(*song, nSamples, &channels, &samplingRate, 
+                            &bitsPerSample, &segmentSize) < 1) 
+    {
+        fprintf( stderr, "Could not get audio format information\n");
+        AIFF_CloseFile(*song);
+        return -1;
+    }
+
+    /* Extract audio samples */
+    int numBytes = (*nSamples * 4 * 2);          // each sample is 4 bytes, AIFF calls a frame a "sample", so we mult by 2
+    *sampleArray = (int *) malloc(numBytes);
+    if (!*sampleArray) {
+        fprintf( stderr, "soundSamples could not be allocated\n"); 
+        return -1;
+    }
+    AIFF_ReadSamples32Bit(*song, *sampleArray, *nSamples * 2);
+
+    return 0;
+}
+
+int cleanSong(int *soundSamples, AIFF_Ref *song) {
+    free(soundSamples);                                 // Free soundSamples
+    AIFF_CloseFile(*song);                               // Close song file  
+    return 0;  
+}
+
+int handlePaError(PaError err) 
+{
     fprintf(stderr, "An Error occured while using the portaudio stream\n");
     fprintf(stderr, "Error Number: %d\n", err);
     fprintf(stderr, "Error message: %s\n", Pa_GetErrorText( err ));
