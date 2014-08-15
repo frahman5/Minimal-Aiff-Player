34f94d7ca82c77c0f6c52a94a1bc8794f03ee6b3
Faiyam Rahman <frahman305@gmail.com>
2014-08-15 17:12:31 -0400

    player is 95% functional. Pa_Terminate still throws seemingly random errors

---
+++ bashtoback.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+## Script to play ByYourSide pt 1 and pt 2 back to back
+
+./play AIFFs/ByYourSidePt1.aiff
+./play AIFFs/ByYourSidePt2.aiff
--- play.c
+++ play.c
@@ -14,17 +14,17 @@ int getAudioData(const char *file, AIFF_Ref *song, int **sampleArray, uint64_t *
 /* Clean song parameters after playing the file */
 int cleanSong(int *soundSamples, AIFF_Ref *song);
 
+/* Using portaudio, stream song to default audio device */
+int streamSong(int *soundSamples, int numSamples);
+
 /* play the AIFF referenced by argv[1]
      Usage: play musicFile.aiff */
 int main(int argc, char *argv[]) {
-
     /* portaudio and AIFF parameters */
     PaError err;
-    PaStream *stream;
     AIFF_Ref song;
-    int buffer[FRAMES_PER_BUFFER][2];           // stereo output buffer
     
-    /* Open the songfile and get sound Samples */
+    /* Get song data */
     int *soundSamples;
     uint64_t numSamples;
     const char *songFile = argv[1];
@@ -35,66 +35,16 @@ int main(int argc, char *argv[]) {
     }
     printf("We got audio data succesfully\n");
 
-    /* Open a stream to play the song */
-    if ((err = Pa_Initialize()) != paNoError ) 
-        handlePaError(err);
-    err = Pa_OpenDefaultStream(
-            &stream,                            // stream
-            0,                                  // numInputChannels. 0->output only stream
-            2,                                  // numOutputChannels
-            paInt32,                            // sample format (song is read in as 32 bit ints)
-            44100,                              // sample rate in hertz,
-            0,                                  // preferred granularity for blocking read/write stream. 0->let portaudio optimize this 
-            NULL,                               // pointer to asynchronous callback function for filling output buffer,
-            NULL);                              // pointer passed to callback function
-    if ( err != paNoError ) 
-        handlePaError(err);      
+    /* Stream song to default audio device */
+    if ((err = streamSong(soundSamples, numSamples)) < 0) handlePaError(err);
 
-    /* Play the song */
-    int loopOffset;
-    int frameOffset;
-    int loopLimit = numSamples / FRAMES_PER_BUFFER;   // on each loop, we go through FRAMES_PER_BUFFER "samples" as defined by AIFF
-    err = Pa_StartStream(stream);
-    if ( err != paNoError ) { handlePaError(err); }
-
-    for (int i = 0; i < loopLimit; i++) {
-        // On the ith iteration, we've already played loopOffset samples
-        loopOffset = (i * 2 * FRAMES_PER_BUFFER); 
-        for (int j = 0; j < FRAMES_PER_BUFFER; j++) // Each iteration, load another frame into the buffer
-        {
-            frameOffset = 2 * j;                    // On jth iteration, we've alreayd read frameOffset frames
-            buffer[j][0] = *(soundSamples + loopOffset + frameOffset);
-            buffer[j][1] = *(soundSamples + loopOffset + frameOffset + 1);
-        }
-        err = Pa_WriteStream(stream, buffer, FRAMES_PER_BUFFER);
-        if ( err != paNoError ) { handlePaError(err); }
-    }
-    /* Clean up (Stop stream, close stream, free soundSamples) */
+    /* Clean up AIFF references and soundSamples */
     cleanSong(soundSamples, &song);
-    if ((err = Pa_StopStream(stream)) != paNoError)     // Stop the stream
-        handlePaError(err); 
-    if ( (err = Pa_CloseStream(stream)) != paNoError )  // Close the stream
-        handlePaError(err);
-    if ( (err = Pa_Terminate()) != paNoError )          // Terminate port audio
-        handlePaError(err);
-    
-    /* Print song info */
-    // seconds = nSamples / samplingRate;
-    // printf("Song Length: %lu:%02lu:%02lu \n", (unsigned long) seconds/360, 
-    //            (unsigned long) (seconds/60)%60, (unsigned long) seconds%60 );
-    // printf("Sampling Rate: %f\n", samplingRate);
-    // printf("Number of Channels: %d\n", channels);
-    // printf("bit depth: %i\n", bitsPerSample);
-    // printf("Num samples: %llu\n", nSamples);
-    // printf("FRAMES_PER_BUFFER: %d\n", FRAMES_PER_BUFFER);
-    // printf("loopLimit: %d\n", loopLimit);
-    // printf("loopLimit as float: %f\n", nSamples / (double) FRAMES_PER_BUFFER);
     
     return 0;
 }
 
-int getAudioData(const char *file, AIFF_Ref *song, int **sampleArray, uint64_t *nSamples)
-{
+int getAudioData(const char *file, AIFF_Ref *song, int **sampleArray, uint64_t *nSamples) {
     /* Open file */
     *song = AIFF_OpenFile(file, F_RDONLY);
     if (!*song) 
@@ -128,14 +78,66 @@ int getAudioData(const char *file, AIFF_Ref *song, int **sampleArray, uint64_t *
     return 0;
 }
 
+int streamSong(int *soundSamples, int numSamples) {
+    PaError err;
+    PaStream *stream;
+    int buffer[FRAMES_PER_BUFFER][2];           // stereo output buffer
+
+    /* Open a stream to play the song */
+    if ((err = Pa_Initialize()) != paNoError ) return err;
+    err = Pa_OpenDefaultStream(
+            &stream,                            // stream
+            0,                                  // numInputChannels. 0->output only stream
+            2,                                  // numOutputChannels
+            paInt32,                            // sample format (song is read in as 32 bit ints)
+            44100,                              // sample rate in hertz,
+            0,                                  // preferred granularity for blocking read/write stream. 0->let portaudio optimize this 
+            NULL,                               // pointer to asynchronous callback function for filling output buffer,
+            NULL);                              // pointer passed to callback function
+    if (err != paNoError) return err;      
+
+    /* Play the song */
+    int loopOffset;
+    int frameOffset;
+    int loopLimit = numSamples / FRAMES_PER_BUFFER;     // on each loop, we go through FRAMES_PER_BUFFER "samples" as defined by AIFF
+    int samplesLeft = numSamples * 2;                   // recall an AIFF sample is actually a frame
+    int numFrames;                                      // number of frames to write to stream
+    if ((err = Pa_StartStream(stream)) != paNoError) return err;
+
+    int counter = 0;
+    while (samplesLeft != 0) 
+    {
+        numFrames = (samplesLeft > FRAMES_PER_BUFFER) ? FRAMES_PER_BUFFER : (samplesLeft / 2);
+        loopOffset = (counter++ * 2 * numFrames); 
+        samplesLeft -= (numFrames * 2);
+
+        for (int j = 0; j < numFrames; j++) // Each iteration, load another frame into the buffer
+        {
+            frameOffset = 2 * j;            // On jth iteration, we've already read frameOffset frames
+            buffer[j][0] = *(soundSamples + loopOffset + frameOffset);
+            buffer[j][1] = *(soundSamples + loopOffset + frameOffset + 1);
+        }
+        err = Pa_WriteStream(stream, buffer, numFrames);
+        if (err != paNoError) return err;
+
+    }
+    
+    /* Clean up shop */
+    if ( ((err = Pa_StopStream(stream)) != paNoError)  ||    // Stop the stream
+         ((err = Pa_CloseStream(stream)) != paNoError) ||    // Close the stream
+         ((err = Pa_Terminate()) != paNoError) )             // Terminate port audio
+        return err; 
+
+    return 0;
+
+}
 int cleanSong(int *soundSamples, AIFF_Ref *song) {
     free(soundSamples);                                 // Free soundSamples
-    AIFF_CloseFile(*song);                               // Close song file  
+    AIFF_CloseFile(*song);                              // Close song file  
     return 0;  
 }
 
-int handlePaError(PaError err) 
-{
+int handlePaError(PaError err) {
     fprintf(stderr, "An Error occured while using the portaudio stream\n");
     fprintf(stderr, "Error Number: %d\n", err);
     fprintf(stderr, "Error message: %s\n", Pa_GetErrorText( err ));
