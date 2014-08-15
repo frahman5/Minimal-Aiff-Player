fa1e9aba84e1c23a3e3790a04879ea42993d42b3
Faiyam Rahman <frahman305@gmail.com>
2014-08-15 15:08:59 -0400

    now playing ByYourSidePt1.aiff, but is still buggy

--- play.c
+++ play.c
@@ -30,23 +30,17 @@ int main(int argc, char *argv[]) {
     printf("Opened file succesfully\n");
 
     /* Print sound format info */
-    uint64_t seconds, nSamples;
-    int channels;
-    double samplingRate;
-    int bitsPerSample;
-    int segmentSize;
+    uint64_t seconds = 0;
+    uint64_t nSamples = 0;
+    int channels = 0;
+    double samplingRate = 0;
+    int bitsPerSample = 0;
+    int segmentSize = 0;
 
     if (AIFF_GetAudioFormat(song, &nSamples, &channels, &samplingRate, &bitsPerSample, &segmentSize) < 1) {
         printf("Could not get audio format information");
         AIFF_CloseFile(song);
     }
-
-    seconds = nSamples / samplingRate;
-    printf("Song Length: %lu:%02lu:%02lu \n", (unsigned long) seconds/360, 
-               (unsigned long) (seconds/60)%60, (unsigned long) seconds%60 );
-    printf("Sampling Rate: %f\n", samplingRate);
-    printf("Number of Channels: %d\n", channels);
-    printf("bit depth: %i\n", bitsPerSample);
 
     /* Fuck around with PortAudio */
     // int error;                          // for capturing Pa Errors
@@ -67,9 +61,9 @@ int main(int argc, char *argv[]) {
                          &stream,           // stream
                          0,                 // numInputChannels. 0->output only stream
                          2,                 // numOutputChannels
-                         paInt24,           // sample format (song has 16 bit depth)
+                         paInt32,           // sample format (song has 16 bit depth but gets read in as 32 bit ints)
                          44100,             // sample rate in hertz,
-                         FRAMES_PER_BUFFER, // preferred granularity for blocking read/write stream. 0->let portaudio optimize this 
+                         0,                 // preferred granularity for blocking read/write stream. 0->let portaudio optimize this 
                          NULL,              // pointer to asynchronous callback function for filling output buffer,
                          NULL);             // pointer passed to callback function
     if ( err != paNoError ) { handlePaError(err); }
@@ -83,32 +77,24 @@ int main(int argc, char *argv[]) {
     int numBytes = (nSamples * 4);          // ReadSamples returns samples in 32 bit ints
     printf("Length of soundSamples in bytes: %d\n", numBytes);
     printf("num Samples: %llu\n", nSamples);
-    int *soundSamples0 = (int *) malloc(numBytes);
-    AIFF_ReadSamples32Bit(song, soundSamples0, nSamples);
-    // int16_t *soundSamples = (int16_t *) malloc(numBytes / 2);
-    int *soundSamples = (int *) malloc(numBytes);
-    int nextSample;
-    for (int k = 0; k < nSamples; k++) {
-        nextSample = (*(soundSamples0 + k) / (int) pow(2,16)) & 0x0000ffff;
+    int *soundSamples = (int *) malloc(2 * numBytes);
+    if (!soundSamples) {
+        fprintf( stderr, "soundSamples could not be allocated\n");
+        abort();
+    }
+    AIFF_ReadSamples32Bit(song, soundSamples, 2 * nSamples);
 
-        if (k < 10) {
-            printf("Massaged sample (without cast): %08x\n", nextSample);
-            printf("Massaged sample (with cast): %08x\n", (int16_t) nextSample);
-        }
-        *(soundSamples + k) = nextSample;
-    }
-    for (int l = 0; l < 10; l++) {
-        printf("Next Sample(32---16): %08x---%08x\n", *(soundSamples0 + l), *(soundSamples + l));
-    }
     int bufferOffset;
-    for (int i = 0; i < nSamples / FRAMES_PER_BUFFER; i++) {
+    int loopLimit = nSamples / (FRAMES_PER_BUFFER);
+    for (int i = 0; i < loopLimit; i++) {
+        printf("we've gone through 1024 frames %d of %d times\n", i, loopLimit);
         // On the ith iteration, we've already played bufferOffset samples
         bufferOffset = (i * 2 * FRAMES_PER_BUFFER);  
+        printf("buffer offset: %d\n", bufferOffset);
         for (int j = 0; j < FRAMES_PER_BUFFER; ) {
             // printf("j: %d\n", j);
-            buffer[j][0] = *(soundSamples0 + bufferOffset + (2 * j) );
-            buffer[j][1] = *(soundSamples0   + bufferOffset + (2 * j) + 1);
-            j++;
+            buffer[j][0] = *(soundSamples + bufferOffset + (2 * j) );
+            buffer[j][1] = *(soundSamples + bufferOffset + (2 * j) + 1);
             j++;
         }
         err = Pa_WriteStream( stream, buffer, FRAMES_PER_BUFFER );
@@ -117,6 +103,20 @@ int main(int argc, char *argv[]) {
 
     err = Pa_StopStream( stream );
     if ( err != paNoError ) { handlePaError(err); }
+
+    /* Clean up the soundSamples array */
+    free(soundSamples);
+
+    seconds = nSamples / samplingRate;
+    printf("Song Length: %lu:%02lu:%02lu \n", (unsigned long) seconds/360, 
+               (unsigned long) (seconds/60)%60, (unsigned long) seconds%60 );
+    printf("Sampling Rate: %f\n", samplingRate);
+    printf("Number of Channels: %d\n", channels);
+    printf("bit depth: %i\n", bitsPerSample);
+    printf("Num samples: %llu\n", nSamples);
+    printf("FRAMES_PER_BUFFER: %d\n", FRAMES_PER_BUFFER);
+    printf("loopLimit: %d\n", loopLimit);
+    printf("loopLimit as float: %f\n", nSamples / (double) (2 * FRAMES_PER_BUFFER));
 
     /* Close the stream once we're done */
     err = Pa_CloseStream(stream);        
@@ -142,5 +142,5 @@ int handlePaError(PaError err) {
         fprintf( stderr, "Host API error = %s\n", hostErrorInfo->errorText);
     }
     Pa_Terminate();
-    return err;
+    abort();
 }
